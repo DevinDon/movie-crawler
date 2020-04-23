377ba8fe43f2846161a946ed4afc090822cfb3f6
Devin Don <I.INF@Outlook.com>
2020-04-23 23:36:56 +0800

    test: add test

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,14 +1,29 @@
-import { asyncHello, hello } from '../main';
+import { Crawler } from '../main';
 
-describe('Hello, world!', () => {
+describe('Test keyword 肥龙过江', () => {
 
-  it('should say `Hello, world!`', done => {
-    expect(hello('world')).toEqual('Hello, world!');
+  beforeAll(done => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30 * 1000;
     done();
   });
 
-  it('should async say `Hello, world!`', async done => {
-    expect(await asyncHello('world')).toEqual('Hello, world!');
+  it('keyword 猛龙过江 should get 1 result', async done => {
+    const crawler = new Crawler();
+    const result = await crawler.search('肥龙过江');
+    expect(result.length).toEqual(1);
+    expect(result[0].url).toEqual('http://www.y80s.com/movie/38197');
+    done();
+  });
+
+  it('movie 肥龙过江 should get 2 download links', async done => {
+    const crawler = new Crawler();
+    const detail = await crawler.getDetail('http://www.y80s.com/movie/38197');
+    expect(detail.download.length).toEqual(2);
+    done();
+  });
+
+  afterAll(done => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 5 * 1000;
     done();
   });
 
