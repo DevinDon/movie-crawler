c32f624479289db8c31b0139aa12022f4ecc1df0
Devin Don <I.INF@Outlook.com>
2020-04-28 03:56:38 +0800

    perf(model/search): 标准化搜索结构

--- src/main/model/search.ts
+++ src/main/model/search.ts
@@ -1,16 +1,14 @@
 export interface Result {
   title: string;
+  image: string;
   url: string;
-  img: string;
-  year: string;
   type: string;
-  /** 英文名称 */
-  ename: string;
+  year: string;
 }
 
 // 详情页面
-// https://www.pianku.tv/mv/wNnZjYidja.html
+// GET https://www.pianku.tv/mv/wNnZjYidja.html
 // 下载地址请求
-// https://www.pianku.tv/ajax/downurl/wNnZjYidja_mv/
+// GET https://www.pianku.tv/ajax/downurl/wNnZjYidja_mv/
 // 搜索请求
-// https://www.pianku.tv/s/ajax.php?q={{keyword}}
+// GET https://www.pianku.tv/s/ajax.php?q={{keyword}}
