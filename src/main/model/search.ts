3b832545d4bca3eed54dd29f10bd724f136f4475
Devin Don <I.INF@Outlook.com>
2020-04-28 03:21:21 +0800

    feat(model/search): 搜索

---
+++ src/main/model/search.ts
@@ -0,0 +1,16 @@
+export interface Result {
+  title: string;
+  url: string;
+  img: string;
+  year: string;
+  type: string;
+  /** 英文名称 */
+  ename: string;
+}
+
+// 详情页面
+// https://www.pianku.tv/mv/wNnZjYidja.html
+// 下载地址请求
+// https://www.pianku.tv/ajax/downurl/wNnZjYidja_mv/
+// 搜索请求
+// https://www.pianku.tv/s/ajax.php?q={{keyword}}
