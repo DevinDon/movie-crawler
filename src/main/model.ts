0aa8b73823a9a3ce8152fc570c8da27d89436372
Devin Don <I.INF@Outlook.com>
2020-04-27 14:28:51 +0800

    perf(model): 优化数据结构并更新注释

--- src/main/model.ts
+++ src/main/model.ts
@@ -3,20 +3,283 @@ export interface SearchResult {
   url: string; // #block1 > ul > li > a [href]
 }
 
+/**
+ * 下载链接 Download
+ *
+ * 从当前页面解析到的源数组
+ *
+ * ```javascript
+ * [...
+ *   document
+ *     .querySelector('#myform > ul')
+ *     .querySelectorAll('.dlname.nm')
+ * ].map(
+ *   ele => ({
+ *     title: ele.querySelector('a').textContent.trim(),
+ *     uri: ele.querySelector('input').value,
+ *     size: ele.querySelector('span').childNodes[4].textContent.trim(),
+ *     type: ele.querySelector('span').childNodes[1].textContent.trim()
+ *   })
+ * )
+ * ```
+ */
 export interface Download {
+  /**
+   * 标题 Title
+   *
+   * ```javascript
+   * ele.querySelector('a').textContent.trim()
+   * ```
+   */
   title: string;
-  url: string;
+  /**
+   * 链接 URI
+   *
+   * ```javascript
+   * ele.querySelector('input').value
+   * ```
+   */
+  uri: string;
+  /**
+   * 大小 Size
+   *
+   * ```javascript
+   * ele.querySelector('span').childNodes[4].textContent.trim()
+   * ```
+   */
   size: string;
+  /**
+   * 类型 Type
+   *
+   * ```javascript
+   * ele.querySelector('span').childNodes[1].textContent.trim()
+   * ```
+   */
+  type: string;
+}
+
+/**
+ * 豆瓣链接 Douban
+ *
+ * 从当前页面解析到的源链接
+ *
+ * ```javascript
+ * document
+ *   .querySelector('#minfo > div.info > div:nth-child(11) > span:nth-child(2) > a')
+ *   .getAttribute('href')
+ *   .match(/(.*)\/comments/)[1]
+ * ```
+ */
+export interface Douban {
+  /**
+   * 豆瓣电影编号 Douban ID
+   *
+   * ```javascript
+   * +link
+   *   .match(/subject\/(.*)/)[1]
+   * ```
+   */
+  id: number;
+  /**
+   * 评论链接
+   *
+   * ```javascript
+   * link + '/comments'
+   * ```
+   */
+  commentLink: string;
+  /**
+   * 豆瓣电影链接 Movie Link
+   *
+   * ```javascript
+   * link
+   * ```
+   */
+  MovieLink: string;
 }
 
 export interface Detail {
-  title: string; // #minfo > div.info > h1
-  image: string; // #minfo > div.img > img
-  artist: string; // #minfo > div.info > div:nth-child(3) > span:nth-child(9) > a
-  desc: string; // #movie_content_all
-  type: string; // #minfo > div.info > div:nth-child(3) > span:nth-child(7) > a
-  area: string; // #minfo > div.info > div:nth-child(3) > span:nth-child(8) > a
-  date: string; // #minfo > div.info > div:nth-child(3) > span:nth-child(10)
-  rate: string; // #minfo > div.info > div:nth-child(4) > div
-  download: Download[]; // #myform > ul > li:not(:first-child):not(:last-child)
+  /**
+   * 源地址，即爬取页面地址。
+   *
+   * Source url.
+   */
+  source: string;
+  /**
+   * 标题 Title
+   *
+   * ```javascript
+   * document
+   *   .querySelector('#minfo > div.info > h1')
+   *   .textContent
+   * ```
+   */
+  title: string;
+  /**
+   * 年份 Year
+   *
+   * ```javascript
+   * document
+   *   .querySelector('#minfo > div.info')
+   *   .childNodes[4]
+   *   .textContent
+   *   .trim()
+   *   .match(/\((.*)\)/)[1]
+   * ```
+   */
+  year: number;
+  /**
+   * 别名 Alias
+   *
+   * ```javascript
+   * document
+   *   .querySelector('#minfo > div.info > span:nth-child(6)')
+   *   .childNodes[2]
+   *   .textContent
+   *   .trim()
+   *   .replace(/ \/ /g, ' , ')
+   *   .split(' , ')
+   * ```
+   */
+  aliases: string[];
+  /**
+   * 演员 Artist
+   *
+   * ```javascript
+   * [...
+   *   document
+   *     .querySelector('#minfo > div.info > span:nth-child(8)')
+   *     .querySelectorAll('a')
+   * ].map(v => v.textContent)
+   * ```
+   */
+  artists: string[];
+  /**
+   * 类型 Type
+   *
+   * ```javascript
+   * [...
+   *   document
+   *     .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(1)')
+   *     .querySelectorAll('a')
+   * ].map(v => v.textContent)
+   * ```
+   */
+  types: string[];
+  /**
+   * 地区 Area
+   *
+   * ```javascript
+   * [...
+   *   document
+   *     .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(2)')
+   *     .querySelectorAll('a')
+   * ].map(v => v.textContent)
+   * ```
+   */
+  areas: string;
+  /**
+   * 语言 Language
+   *
+   * ```javascript
+   * [...
+   *   document
+   *     .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(3)')
+   *     .querySelectorAll('a')
+   * ].map(v => v.textContent)
+   * ````
+   */
+  languages: string[];
+  /**
+   * 导演 Director
+   *
+   * ```javascript
+   * [...
+   *   document
+   *     .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(4)')
+   *     .querySelectorAll('a')
+   * ].map(v => v.textContent)
+   * ````
+   */
+  directors: string[];
+  /**
+   * 上映日期 Release Date
+   *
+   * JavaScript 数字时间戳
+   *
+   * ```javascript
+   * new Date(
+   *   document
+   *     .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(5)')
+   *     .childNodes[1]
+   *     .textContent
+   * ).getTime()
+   * ```
+   */
+  releaseDate: number;
+  /**
+   * 页面更新日期 Update Date
+   *
+   * JavaScript 数字时间戳
+   *
+   * ```javascript
+   * new Date(
+   *   document
+   *     .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(7)')
+   *     .childNodes[1]
+   *     .textContent
+   * ).getTime()
+   * ```
+   */
+  updateDate: number;
+  /**
+   * 片长 Duration
+   *
+   * 分钟 Minutes
+   *
+   * ```javascript
+   * +document
+   *   .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(6)')
+   *   .childNodes[1]
+   *   .textContent.trim()
+   *   .match(/(.*)分钟/)[1]
+   * ```
+   */
+  duration: number;
+  /**
+   * 电影评分 Rating
+   *
+   * ```javascript
+   * +document
+   *   .querySelector('#minfo > div.info > div:nth-child(11) > span:nth-child(1)')
+   *   .childNodes[4]
+   *   .textContent
+   *   .trim()
+   * ```
+   */
+  rating: number;
+  /**
+   * 简介 Description
+   *
+   * ```javascript
+   * document
+   *   .querySelector('#movie_content')
+   *   .childNodes[2]
+   *   .textContent
+   *   .trim()
+   * ```
+   */
+  description: string;
+  /**
+   * 豆瓣信息 Douban
+   *
+   * 见上
+   */
+  douban: Douban;
+  /**
+   * 下载链接 Download
+   *
+   * 见上
+   */
+  downloads: Download[];
 }
