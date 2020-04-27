4398a5fc7351e87d9e1e286b93ad81b8004a2353
Devin Don <I.INF@Outlook.com>
2020-04-27 15:49:34 +0800

    test(index): test constructor

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,29 +1,10 @@
 import { Crawler } from '../main';
 
-describe('Test keyword 肥龙过江', () => {
+describe('Test Constructor', () => {
 
-  beforeAll(done => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30 * 1000;
-    done();
-  });
-
-  it('keyword 猛龙过江 should get 1 result', async done => {
+  it('should create a new Crawler', async done => {
     const crawler = new Crawler();
-    const result = await crawler.search('肥龙过江');
-    expect(result.length).toEqual(1);
-    expect(result[0].url).toEqual('http://www.y80s.com/movie/38197');
-    done();
-  });
-
-  it('movie 肥龙过江 should get 2 download links', async done => {
-    const crawler = new Crawler();
-    const detail = await crawler.getDetail('http://www.y80s.com/movie/38197');
-    expect(detail.download.length).toEqual(2);
-    done();
-  });
-
-  afterAll(done => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 5 * 1000;
+    expect(crawler).toBeDefined();
     done();
   });
 
