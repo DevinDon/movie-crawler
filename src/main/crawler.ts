0f385e25bbfc834736b0d5a39bd48daf3dd66772
Devin Don <I.INF@Outlook.com>
2020-04-27 14:59:17 +0800

    feat(crawler): 更新解析方式

--- src/main/crawler.ts
+++ src/main/crawler.ts
@@ -4,17 +4,22 @@ import { Detail, Download, SearchResult } from './model';
 
 export class Crawler {
 
-  private base = 'http://www.y80s.com';
+  private host = 'www.80s.tw';
+  private base = 'https://www.80s.tw';
 
-  private header = {
-    Host: 'www.y80s.com',
-    Origin: 'http://www.y80s.com',
-    Referer: 'http://www.y80s.com/'
-  };
+  private header: any;
 
-  constructor() { }
+  constructor() {
+    this.header = {
+      Host: this.host,
+      Origin: this.base,
+      Referer: this.base
+    };
+  }
 
   /**
+   * 通过关键字搜索，返回一个搜索结果数组。
+   *
    * Search by keyword, return an array.
    *
    * @param {string} keyword Search keyword.
@@ -39,6 +44,8 @@ export class Crawler {
   }
 
   /**
+   * 通过详情 URL 页面解析电影信息。
+   *
    * Get movie detail with detail page url.
    *
    * @param {string} url Url of movie detail page.
@@ -50,21 +57,100 @@ export class Crawler {
       .set(this.header);
 
     const document = new JSDOM(response.text).window.document;
+
+    const doubanID = document
+      .querySelector('#minfo > div.info > div:nth-child(11) > span:nth-child(2) > a')!
+      .getAttribute('href')!
+      .match(/subject\/(.*)\/comments/)![1];
+
     const detail: Detail = {
-      title: document.querySelector('#minfo > div.info > h1')?.textContent?.trim()!,
-      image: document.querySelector('#minfo > div.img > img')?.getAttribute('src')!,
-      artist: document.querySelector('#minfo > div.info > div:nth-child(3) > span:nth-child(9) > a')?.textContent?.trim()!,
-      desc: document.querySelector('#movie_content_all')?.textContent?.trim()!,
-      type: document.querySelector('#minfo > div.info > div:nth-child(3) > span:nth-child(7) > a')?.textContent?.trim()!,
-      area: document.querySelector('#minfo > div.info > div:nth-child(3) > span:nth-child(8) > a')?.textContent?.trim()!,
-      date: document.querySelector('#minfo > div.info > div:nth-child(3) > span:nth-child(10)')?.textContent?.trim()!,
-      rate: document.querySelector('#minfo > div.info > div:nth-child(8) > div')?.textContent?.trim()!,
-      download: [...document.querySelectorAll('#myform > ul > li:not(:first-child):not(:last-child)')]
-        .map<Download>(ele => ({
-          title: ele.querySelector('.dlname')?.textContent?.trim()!,
-          url: ele.querySelector('[title=本地下载]')?.getAttribute('href')!,
-          size: ''
-        }))
+      source: url,
+      title: document
+        .querySelector('#minfo > div.info > h1')!
+        .textContent!,
+      year: +document
+        .querySelector('#minfo > div.info')!
+        .childNodes[4]
+        .textContent!
+        .trim()
+        .match(/\((.*)\)/)![1],
+      aliases: document
+        .querySelector('#minfo > div.info > span:nth-child(6)')!
+        .childNodes[2]
+        .textContent!
+        .trim()
+        .replace(/ \/ /g, ' , ')
+        .split(' , '),
+      artists: [...
+        document
+          .querySelector('#minfo > div.info > span:nth-child(8)')!
+          .querySelectorAll('a')
+      ].map(v => v.textContent!),
+      types: [...
+        document
+          .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(1)')!
+          .querySelectorAll('a')
+      ].map(v => v.textContent!),
+      areas: [...
+        document
+          .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(2)')!
+          .querySelectorAll('a')
+      ].map(v => v.textContent!),
+      languages: [...
+        document
+          .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(3)')!
+          .querySelectorAll('a')
+      ].map(v => v.textContent!),
+      directors: [...
+        document
+          .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(4)')!
+          .querySelectorAll('a')
+      ].map(v => v.textContent!),
+      releaseDate: new Date(
+        document
+          .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(5)')!
+          .childNodes[1]
+          .textContent!
+      ).getTime(),
+      updateDate: new Date(
+        document
+          .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(7)')!
+          .childNodes[1]
+          .textContent!
+      ).getTime(),
+      duration: +document
+        .querySelector('#minfo > div.info > div:nth-child(10) > span:nth-child(6)')!
+        .childNodes[1]
+        .textContent!
+        .trim()
+        .match(/(.*)分钟/)![1],
+      rating: +document
+        .querySelector('#minfo > div.info > div:nth-child(11) > span:nth-child(1)')!
+        .childNodes[4]
+        .textContent!
+        .trim(),
+      description: document
+        .querySelector('#movie_content')!
+        .childNodes[2]
+        .textContent!
+        .trim(),
+      douban: {
+        id: +doubanID,
+        commentLink: `https://movie.douban.com/subject/${doubanID}/comments`,
+        MovieLink: `https://movie.douban.com/subject/${doubanID}`
+      },
+      downloads: [...
+        document
+          .querySelector('#myform > ul')!
+          .querySelectorAll('.dlname.nm')
+      ].map(
+        ele => ({
+          title: ele.querySelector('a')!.textContent!.trim(),
+          uri: ele.querySelector('input')!.value,
+          size: ele.querySelector('span')!.childNodes[4].textContent!.trim(),
+          type: ele.querySelector('span')!.childNodes[1].textContent!.trim()
+        })
+      )
     };
 
     return detail;
